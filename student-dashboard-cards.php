adb823de77340be4decb29f2fa1711ba9acfb9e3
jangap411 <angapj@gmail.com>
2021-11-26 18:38:48 +1000

    added student test interface

--- student-dashboard-cards.php
+++ student-dashboard-cards.php
@@ -1,11 +1,24 @@
+<?php
+    // $total_std = getTotalNumbers();
+    $sqlGetTotalCourseTaken = "SELECT COUNT(`enrollment`.`studentId`) FROM `Courses`,`enrollment`,`users` WHERE `Courses`.`CourseID`=`enrollment`.`courseID` AND `users`.`UserID`=`enrollment`.`studentId` AND `enrollment`.`studentId`=1;";
+    $fieldName_courseCount = 'COUNT(`enrollment`.`studentId`)';
+
+    $sqlGetTotalCourses = "SELECT COUNT(`CourseID`) FROM `Courses`;";
+    $fieldName_courses = 'COUNT(`CourseID`)';
+
+    $sqlGetTotalTrainers = "SELECT COUNT(`UserID`) FROM `users` WHERE `UserType`=2;";
+    $fieldName_trainers = 'COUNT(`UserID`)';
+?>
 <div class="col-xl-3 col-md-6 mb-4">
     <div class="card border-left-primary shadow h-100 py-2">
         <div class="card-body">
             <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                        My Courses</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">1/5</div>
+                        Courses Taken</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                    <?php echo getTotalNumbers($sqlGetTotalCourseTaken,$fieldName_courseCount) ?>/5
+                    </div>
                 </div>
                 <div class="col-auto">
                     <!-- <i class="fas fa-dollar-sign fa-2x text-gray-300"></i> -->
@@ -23,8 +36,10 @@
             <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
                     <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                        Total Number of Courses</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">500</div>
+                        Total Number of Courses Available</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                        <?php echo getTotalNumbers($sqlGetTotalCourses,$fieldName_courses) ?>
+                    </div>
                 </div>
                 <div class="col-auto">
                     <i class="fas fa-book fa-2x text-gray-300"></i>
@@ -69,7 +84,9 @@
                 <div class="col mr-2">
                     <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
                         Number of Trainers</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                        <?php echo getTotalNumbers($sqlGetTotalTrainers,$fieldName_trainers) ?>
+                    </div>
                 </div>
                 <div class="col-auto">
                     <i class="fas fa-chalkboard-teacher fa-2x text-gray-300"></i>
---
+++ test-matching.php
@@ -0,0 +1,46 @@
+
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">
+            Matching 
+        </h6>
+    </div>
+    <div class="card-body">
+        <p>
+            <h5>Question 1</h2>
+            <P></P>
+            <div class="row p-3">
+                <div style="width: 50rem;">
+                    <p>
+                        SB Admin 2 makes extensive use of Bootstrap 4 utility classes in order to reduce CSS bloat and poor page performance
+                    </p>
+                </div>
+                <div style="width: 50rem; background-color:#d1d0cd;text-align:center;" class="ml-3">
+                    <p>
+                        Drop Here
+                    </p>
+                </div>
+            </div>
+        
+        </p>
+        <p class="mb-0">
+           <!-- <div class="row p-3">
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                    <strong>A.</strong>
+                    <label class="form-check-label" for="flexRadioDefault1">
+                        Default radio
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input mr-5" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                    <strong>B.</strong>
+                    <label class="form-check-label" for="flexRadioDefault1">
+                        Default radio
+                    </label>
+                    
+                </div>
+            </div> -->
+        </p>
+    </div>
+</div>
